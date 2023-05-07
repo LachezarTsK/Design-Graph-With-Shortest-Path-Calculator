7c38f47f9ac39d76e1422004026ac93cd04e08a8
Lachezar Kutsarov <lkutsarov@gmail.com>
2023-05-07 11:23:18 +0200

    Update Solution.cpp

--- Solution.cpp
+++ Solution.cpp
@@ -8,29 +8,29 @@ using namespace std;
 struct Node {
     int ID;
     int costOfEdge;
-    Node(int ID, int costOfEdge): ID {ID}, costOfEdge {costOfEdge}{}
+    Node(int ID, int costOfEdge) : ID {ID}, costOfEdge {costOfEdge} {}
 };
 
 struct Step {
     int ID;
     int costFromStart;
-    Step(int ID, int costFromStart): ID {ID}, costFromStart {costFromStart}{}
+    Step(int ID, int costFromStart) : ID {ID}, costFromStart {costFromStart} {}
 };
 
 class Graph {
-    
+
     const static int NO_PATH_EXISTS = -1;
-    vector<vector<unique_ptr<Node>>> graph;
+    vector<vector<Node>> graph;
     int totalNodes;
 
 public:
-    Graph(int totalNodes, const vector<vector<int>>& edges): totalNodes{totalNodes} {
+    Graph(int totalNodes, const vector<vector<int>>& edges) : totalNodes {totalNodes} {
         graph.resize(totalNodes);
         for (const auto& edge : edges) {
             int from = edge[0];
             int to = edge[1];
             int cost = edge[2];
-            graph[from].push_back(make_unique<Node>(to, cost));
+            graph[from].emplace_back(to, cost);
         }
     }
 
@@ -38,18 +38,18 @@ public:
         int from = edge[0];
         int to = edge[1];
         int cost = edge[2];
-        graph[from].push_back(make_unique<Node>(to, cost));
+        graph[from].emplace_back(to, cost);
     }
 
     int shortestPath(int start, int goal) const {
         vector<int>distanceFromStart(totalNodes, INT_MAX);
         distanceFromStart[start] = 0;
 
-        static auto comparator = [](const Step& first, const Step& second) 
+        static auto comparator = [](const Step& first, const Step& second)
         {return first.costFromStart > second.costFromStart;};
-        
+
         priority_queue<Step, vector<Step>, decltype(comparator)> minHeap(comparator);
-        minHeap.push(Step(start, 0));
+        minHeap.emplace(start, 0);
 
         while (!minHeap.empty()) {
 
@@ -59,11 +59,11 @@ public:
                 break;
             }
 
-            const vector<unique_ptr<Node>>& neighbours = graph[current.ID];
+            const vector<Node>& neighbours = graph[current.ID];
             for (const auto& next : neighbours) {
-                if (distanceFromStart[next->ID] > current.costFromStart + next->costOfEdge) {
-                    distanceFromStart[next->ID] = current.costFromStart + next->costOfEdge;
-                    minHeap.push(Step(next->ID, distanceFromStart[next->ID]));
+                if (distanceFromStart[next.ID] > current.costFromStart + next.costOfEdge) {
+                    distanceFromStart[next.ID] = current.costFromStart + next.costOfEdge;
+                    minHeap.emplace(next.ID, distanceFromStart[next.ID]);
                 }
             }
         }
